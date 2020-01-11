25cd64fe22c6ad8d6db1b8ab9972549b6f589b5c
saturday_study <doguri1031@gmail.com>
2020-01-12 02:49:33 +0900

    add AssignmentOperators

---
+++ src/day1/AssignmentOperators.java
@@ -0,0 +1,34 @@
+package day1;
+
+public class AssignmentOperators {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+		int c = 10;
+		int d = 10;
+		int e = 10;
+		int f = 10;
+		int g = 10;
+		int h = 10;
+		int i = 10;
+		int j = 11;
+		
+		a += b;   // a = a + b
+		System.out.println(a);
+
+		c -= d; // c = c - d
+		System.out.println(c);
+		
+		e *= f; // e = e * f
+		System.out.println(e);
+		
+		g /= h; // g = g / h
+		System.out.println(g);
+		
+		i %= j; // i = i % j
+		System.out.println(i);
+
+	}
+
+}
