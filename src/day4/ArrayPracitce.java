ccb27c2db6d9ecfb7eee98ffb78a7238c90a0cd6
lee <lee@192.168.3.12>
2020-02-02 01:30:04 +0900

    saturday day4

---
+++ src/day4/ArrayPracitce.java
@@ -0,0 +1,19 @@
+package day4;
+
+public class ArrayPracitce {
+
+	public static void main(String[] args) {
+
+		String[] stringArray = new String[3];
+		
+		stringArray[0] = "array1";
+		stringArray[1] = "array2";
+		stringArray[2] = "array3";
+		
+		for (int i = 0; i < stringArray.length; i++) {
+			System.out.println(stringArray[i]);
+		}
+		
+	}
+
+}
